aa4b348d5eb3b5a9bcf31c21cd820daa5c76aaf0
Harshit Kumar <harshitkumar9030@gmail.com>
2024-08-16 08:49:52 +0530

    okay T_T

--- server.ts
+++ server.ts
@@ -1,17 +1,25 @@
-// server.ts
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import callLogsRouter from './routes/callLogs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error('MONGO_URI is not defined in the environment variables');
+}
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/api/call-logs', callLogsRouter);
 
-mongoose.connect('mongodb://localhost:27017/calllogsdb');
+mongoose.connect(mongoUri);
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
